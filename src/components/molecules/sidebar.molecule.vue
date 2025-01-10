6d81e668bcd0262153c76b104d72343f163de144
houstonlws <houstonhvrd@gmail.com>
2025-01-10 15:09:30 -0500

    changed sidebar collapse logic

--- src/components/molecules/sidebar.molecule.vue
+++ src/components/molecules/sidebar.molecule.vue
@@ -21,9 +21,11 @@ watch(
   (newValue) => {
     if (sidebar.value !== null) {
       if (newValue) {
-        sidebar.value.style.width = '250px';
+        sidebar.value.classList.remove('sidebar-closed');
+        sidebar.value.classList.add('sidebar-open');
       } else {
-        sidebar.value.style.width = '0';
+        sidebar.value.classList.remove('sidebar-open');
+        sidebar.value.classList.add('sidebar-closed');
       }
     }
   }
@@ -38,10 +40,35 @@ onClickOutside(sidebar, () => emit('clickOutside'));
     position: fixed
     top: 0
     height: 100%
-    width: 0
     background-color: #fff
     transition: ease-in-out 0.3s
     box-shadow: 0 8px 8px 0 #00000020
+    &-closed
+      width: 0
+      &::before
+        content: ''
+        position: absolute
+        pointer-events: none
+        transition: ease-in-out 0.3s
+        top: 0
+        left: 0
+        opacity: 0
+        width: 100vw
+        height: 100vh
+        background-color: #00000050
+    &-open
+      width: 250px
+      &::before
+        content: ''
+        pointer-events: none
+        opacity: 100
+        position: absolute
+        transition: ease-in-out 0.3s
+        top: 0
+        left: 250px
+        width: 100vw
+        height: 100vh
+        background-color: #00000050
 
 .close-button
   position: absolute
